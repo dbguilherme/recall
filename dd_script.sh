7188eadfc15320717f25457accf8e4e3d7ef7fd3
dbguilherme <dbguilherme@gmail.com>
2018-06-13 13:16:55 -0300

    bug corrections

--- dd_script.sh
+++ dd_script.sh
@@ -6,10 +6,10 @@ sample_input=$1
  while IFS='' read -r line || [[ -n $line ]]; do
  #echo $line
     
-    add=`echo $line | ../kissdb/indexer ../kissdb/201.db 69 25000`
+    add=`echo $line | cut -d' ' -f2 | ../kissdb/indexer ../kissdb/201.db 69 25000`
     #echo $add
     
-    dd if=/home/guilherme/Downloads/tar-toolkit-core-copy/logistic_short/svm.fil ibs=1 skip=$add count=241957 status=none | grep "^legal" | cut -d$'\n' -f1 
+    dd if=svm.fil ibs=1 skip=$add count=241957 status=none | grep "^legal" | cut -d$'\n' -f1 
  
  
  done < $sample_input
